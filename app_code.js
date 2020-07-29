3f159f6247a291412b1d1a05872745b7483fed40
Jozef_Scensny <jozef.scensny@outlook.com>
2020-07-29 20:46:52 +0200

    Added function numberClicked(val) which is run when any digit on a calculator is clicked. Added also empty functions (operationPlusClicked(), operationClicked(), resultClicked()) to enable debugging - these can be rewritten. Changed calls in all addEventListener functions, which had invalid syntax.

--- app_code.js
+++ app_code.js
@@ -5,13 +5,7 @@
 
 
 // DOM variables
-var txtWelcome = document.getElementById("txtWelcome")
-,   btnNewGame = document.getElementById("btnNewGame")
-,   btnHit = document.getElementById("btnHit")
-,   btnStay = document.getElementById("btnStay")
-,   pPlayerBase = document.getElementById("playerBase")
-,   pDealerBase = document.getElementById("dealerBase")
-,   pNotificationBar = document.getElementById("notificationBar")
+var txt_display = document.getElementById("txt_display");
 
 //var undefinedVal = -1;
 //var valueStore = [undefinedVal, undefinedVal];
@@ -19,23 +13,23 @@ var value;
 var isOperationSelected = false;
 
 
-btn_val_0.addEventListener('click', numberClicked (0));
-btn_val_1.addEventListener('click', numberClicked (1));
-btn_val_2.addEventListener('click', numberClicked (2));
-btn_val_3.addEventListener('click', numberClicked (3));
-btn_val_4.addEventListener('click', numberClicked (4));
-btn_val_5.addEventListener('click', numberClicked (5));
-btn_val_6.addEventListener('click', numberClicked (6));
-btn_val_7.addEventListener('click', numberClicked (7));
-btn_val_8.addEventListener('click', numberClicked (8));
-btn_val_9.addEventListener('click', numberClicked (9));
+btn_val_0.addEventListener('click', function () {numberClicked (0)}, false);
+btn_val_1.addEventListener('click', function () {numberClicked (1)}, false);
+btn_val_2.addEventListener('click', function () {numberClicked (2)}, false);
+btn_val_3.addEventListener('click', function () {numberClicked (3)}, false);
+btn_val_4.addEventListener('click', function () {numberClicked (4)}, false);
+btn_val_5.addEventListener('click', function () {numberClicked (5)}, false);
+btn_val_6.addEventListener('click', function () {numberClicked (6)}, false);
+btn_val_7.addEventListener('click', function () {numberClicked (7)}, false);
+btn_val_8.addEventListener('click', function () {numberClicked (8)}, false);
+btn_val_9.addEventListener('click', function () {numberClicked (9)}, false);
 
-btn_op_plus.addEventListener('click', operationClicked ('+'));
-btn_op_minus.addEventListener('click', operationClicked ('-'));
-btn_op_multiply.addEventListener('click', operationClicked ('*'));
-btn_op_divide.addEventListener('click', operationClicked ('/'));
+btn_op_plus.addEventListener('click', function () {operationPlusClicked ()}, false);
+btn_op_minus.addEventListener('click', function () {operationClicked ('-')}, false);
+btn_op_multiply.addEventListener('click', function () {operationClicked ('*')}, false);
+btn_op_divide.addEventListener('click', function () {operationClicked ('/')}, false);
 
-btn_op_divide.addEventListener('click', resultClicked ());
+btn_op_divide.addEventListener('click', function () {resultClicked ()}, false);
 
 
 // ----------------------
@@ -44,13 +38,44 @@ btn_op_divide.addEventListener('click', resultClicked ());
 
 function numberClicked ( val ) {
 
-    if ( !isOperationSelected) {
-        value = val;
+    // if ( isOperationSelected == true ) {
+    //     txt_display.innerHTML = '';
+    // }
+    console.log('numberClicked started with val', val);
+    
+    var onDisplayNow = txt_display.innerHTML;
+    if (onDisplayNow.length == 1 && onDisplayNow == '0') {
+
+        if (val == '0') {
+            return;
+        }
+        else {
+            onDisplayNow = val;
+        }
     }
     else {
-        
+        onDisplayNow = onDisplayNow.concat(val);
     }
+    
+    txt_display.innerHTML = onDisplayNow;
     
 }
 
 
+
+function operationPlusClicked () {
+
+
+}
+
+
+function operationClicked (val) {
+
+
+}
+
+
+
+function resultClicked () {
+
+}
